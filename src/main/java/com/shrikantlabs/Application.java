d176c8d0f733add290ec90bfa76d86f58142ab53
shrikant <shrikantdeshmukh2008@gmail.com>
2025-01-05 17:45:49 +0530

    ECOM234 fix

--- src/main/java/com/shrikantlabs/Application.java
+++ src/main/java/com/shrikantlabs/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("This is first application");
+		//changes for ECOM-333
 	}
 
 }
