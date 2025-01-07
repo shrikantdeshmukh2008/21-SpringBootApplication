1db65a6901ef33e257781e023d8c74f260b33166
shrikant <shrikantdeshmukh2008@gmail.com>
2025-01-07 12:26:36 +0530

    Changes of ECOM-300 added

--- src/main/java/com/shrikantlabs/Application.java
+++ src/main/java/com/shrikantlabs/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+	private static final String REST_URL="https://www.fleetfarm.com";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
