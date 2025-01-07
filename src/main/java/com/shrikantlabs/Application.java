350ea65bcff80d675911036eb55bfc9c08b8c427
shrikant <shrikantdeshmukh2008@gmail.com>
2025-01-07 12:40:53 +0530

    ECOM-301 changes

--- src/main/java/com/shrikantlabs/Application.java
+++ src/main/java/com/shrikantlabs/Application.java
@@ -5,12 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+	private Map<String,Object> cache=new HashMap<String,Object>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("This is first application");
 		//changes for ECOM-333
 		int a=10;
+	}
+	//ECOM-301 changes
+	public void loadDataToCache(){
+		//business logic
 	}
 
 }
